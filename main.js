e139db89397c2f9a66b993eb9b2f9b639ef4142f
John <cafetiza@gmail.com>
2021-10-13 03:31:27 -0500

    Skills dinamics

--- main.js
+++ main.js
@@ -4,39 +4,139 @@ const skills = document.querySelector('.habilidades');
 const contact = document.querySelector('.contactame');
 const cv = document.querySelector('.cv');
 const numero = document.querySelector('.num');
-var cv1 = "https://drive.google.com/file/d/11NGO8rknhUW4BgRJkbhS1-aKyEkGgXLa/view?usp=sharing";
-var cv2
+const developer = document.querySelector('.containerD');
+const dataA = document.querySelector('.containerA');
+
+const progress = document.querySelectorAll(".progress");
 
 document.querySelector('#about').onclick = function () {
-    about.style = "opacity: 1; transition: 4s;";
-    skills.style = "opacity: 0; transition: 0.5s;";
-    contact.style = "opacity: 0; transition: 0.5s;";
-    cv.style = "opacity: 0; transition: 0.5s;";
-    /* document.querySelector('#about').style.color = "black"; */
+    setTimeout(function () {
+        about.style = "visibility: visible;";
+    }, 220);
+
+    setTimeout(function () {
+        skills.style = "visibility: hidden; ";
+        contact.style = "visibility: hidden;";
+        cv.style = "visibility: hidden;";
+        numero.style = "visibility: hidden;";
+        developer.style = "visibility: hidden;";
+        dataA.style = "visibility: hidden;";
+    }, 200);
+
+    setTimeout(() => {
+        dataA.style = "opacity: 0;";
+        developer.style = "opacity: 0;";
+        skills.style = "visibility: hidden;";
+    }, .005);
 }
+
 document.querySelector('#skills').onclick = function () {
-    skills.style = "opacity: 1; transition: 4s;";
-    about.style = "opacity: 0; transition: 0.5s;";
-    contact.style = "opacity: 0; transition: 0.5s;";
-    cv.style = "opacity: 0; transition: 0.5s;";
+    setTimeout(function () {
+        skills.style = "visibility: visible;";
+    }, 220);
+
+    setTimeout(function () {
+        about.style = "visibility: hidden;";
+        contact.style = "visibility: hidden;";
+        cv.style = "visibility: hidden;";
+        numero.style = "visibility: hidden;";
+    }, 200);
+
 }
+
 document.querySelector('#contact').onclick = function () {
-    contact.style = "opacity: 1; transition: 4s;";
-    skills.style = "opacity: 0; transition: 0.5s;";
-    about.style = "opacity: 0; transition: 0.5s;";
-    cv.style = "opacity: 0; transition: 0.5s;";
+    setTimeout(function () {
+        contact.style = "visibility: visible;";
+    }, 220);
+
+    setTimeout(function () {
+        cv.style = "visibility: hidden;";
+        skills.style = "visibility: hidden;";
+        about.style = "visibility: hidden;";
+        numero.style = "visibility: hidden;";
+        developer.style = "visibility: hidden;";
+        dataA.style = "visibility: hidden;";
+    }, 200);
+
+    setTimeout(() => {
+        dataA.style = "opacity: 0;";
+        developer.style = "opacity: 0;";
+        skills.style = "visibility: hidden;";
+    }, .005);
 }
 
 document.querySelector('#cv').onclick = function () {
-    cv.style = "opacity: 1; transition: 4s;";
-    contact.style = "opacity: 0; transition: 0.5s;";
-    skills.style = "opacity: 0; transition: 0.5s;";
-    about.style = "opacity: 0; transition: 0.5s;";
+
+    setTimeout(function () {
+        cv.style = "visibility: visible;";
+    }, 220);
+
+    setTimeout(function () {
+        contact.style = "visibility: hidden;";
+        skills.style = "visibility: hidden;";
+        about.style = "visibility: hidden;";
+        numero.style = "visibility: hidden;";
+        developer.style = "visibility: hidden;";
+        dataA.style = "visibility: hidden;";
+    }, 200);
+
+    setTimeout(() => {
+        dataA.style = "opacity: 0;";
+        developer.style = "opacity: 0;";
+        skills.style = "visibility: hidden;";
+    }, .005);
 }
 
 /*Numero*/
 document.querySelector('.fa-whatsapp').onclick = function () {
-    numero.style = "opacity: 1; transition: 2s;";
+    numero.style = "visibility: visible; transition: 4s;";
 }
 
+/*menu Skills*/
+document.querySelector('#Developer').onclick = function () {
+
+    setTimeout(() => {
+        dataA.style = "opacity: 0; visibility: hidden;";
+    }, .005);
+
+    setTimeout(() => {
+        developer.style = "opacity: 1; visibility: visible;";
+
+        progress[0].style = `background: linear-gradient(45deg, #1fe6ff,#673AB7); 
+                            animation: animate 4s ease-in-out forwards;
+                            width:90%;`;
+        progress[1].style = `background: linear-gradient(45deg, #1fe6ff,#673AB7); 
+                            animation: animate 4s ease-in-out forwards;
+                            width:70%;`;
+        progress[2].style = `background: linear-gradient(45deg, #1fe6ff,#673AB7); 
+                            animation: animate 4s ease-in-out forwards;
+                            width:70%;`;
+        progress[3].style = `background: linear-gradient(45deg, #1fe6ff,#673AB7); 
+                            animation: animate 4s ease-in-out forwards;
+                            width:50%;`;
+        progress[4].style = `background: linear-gradient(45deg, #1fe6ff,#673AB7); 
+                            animation: animate 4s ease-in-out forwards;
+                            width:85%;`;
+    }, 10);
+}
+
+document.querySelector('#DataA').onclick = function () {
+    setTimeout(() => {
+        developer.style = "opacity: 0; visibility: hidden;";
+    }, .005);
+
+    setTimeout(() => {
+        dataA.style = "opacity: 1; visibility: visible;";
+
+        progress[5].style = `background: linear-gradient(45deg, #a9ff1f,#b73a3a); 
+                            animation: animate 4s ease-in-out forwards;
+                            width:70%;`;
+        progress[6].style = `background: linear-gradient(45deg, #a9ff1f,#b73a3a); 
+                            animation: animate 4s ease-in-out forwards;
+                            width:70%;`;
+        progress[7].style = `background: linear-gradient(45deg, #a9ff1f,#b73a3a); 
+                            animation: animate 4s ease-in-out forwards;
+                            width:90%;`;
+    }, 10);
+}
 
