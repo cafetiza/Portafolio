cc48359144fa2f05223cce3efb08a3ad464f36f3
John <cafetiza@gmail.com>
2022-01-27 21:44:09 -0600

    restructura de proyectos

--- main.js
+++ main.js
@@ -207,5 +207,58 @@ BtnA.onclick = function () {
     
 }
 
+//popup´s//
+/*CARFT CIRLI*/ 
+var btnAbrirPopup = document.getElementById("btn-abrir-popup"),
+    overlay = document.getElementById("overlay"),
+    popup = document.getElementById("popup"),
+    btnCerrarPopup = document.getElementById("btn-cerrar-popup");
+
+
+btnAbrirPopup.addEventListener("click",function () {
+    overlay.classList.add('active');
+    popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener("click",function () {
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+
+/*Random image*/ 
+var btnAbrirPopup2 = document.getElementById("btn-abrir-popup2"),
+    overlay2 = document.getElementById("overlay2"),
+    popup2 = document.getElementById("popup2"),
+    btnCerrarPopup2 = document.getElementById("btn-cerrar-popup2");
+
+
+btnAbrirPopup2.addEventListener("click",function () {
+    overlay2.classList.add('active');
+    popup2.classList.add('active');
+});
+
+btnCerrarPopup2.addEventListener("click",function () {
+    overlay2.classList.remove('active');
+    popup2.classList.remove('active');
+});
+
+/*Montoya and john*/
+var btnAbrirPopup3 = document.getElementById("btn-abrir-popup3"),
+    overlay3 = document.getElementById("overlay3"),
+    popup3 = document.getElementById("popup3"),
+    btnCerrarPopup3 = document.getElementById("btn-cerrar-popup3");
+
+
+btnAbrirPopup3.addEventListener("click",function () {
+    overlay3.classList.add('active');
+    popup3.classList.add('active');
+});
+
+btnCerrarPopup3.addEventListener("click",function () {
+    overlay3.classList.remove('active');
+    popup3.classList.remove('active');
+});
+
 
 
